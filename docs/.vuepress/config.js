43fb9fea3035aac50eedd3fdaa8d2b25b347eb68
Владислав Люминарский <Vladislav.Lyuminarskiy@gmail.com>
2018-07-05 16:37:31 +0400

    Изменение структуры файлов.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,35 @@
 module.exports = {
-  title: 'Hello VuePress',
-  description: 'Just playing around',
-  base: '/C-course-website/'
+  base: '/C-course-website/',
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: 'The C Programming Language Course',
+      description: 'Educational materials for course "The C Programming Language".'
+    }
+  },
+  themeConfig: {
+    sidebar: [
+      '/',
+      '/practice/lesson01',
+      '/practice/lesson02',
+      '/practice/lesson03',
+      '/practice/lesson04',
+      '/practice/lesson05',
+      '/practice/lesson06',
+      '/practice/lesson07',
+      '/practice/lesson08',
+      '/practice/lesson09',
+      '/practice/lesson10',
+    ]
+  },
+  markdown: {
+    lineNumbers: true,
+    toc: {
+      listType: "ol"
+    },
+    config(md) {
+      md.use(require('markdown-it-katex'));
+    }
+  },
+  serviceWorker: true
 }
